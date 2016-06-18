ba03a99a96e67f1aab7c1a97cb0a2a110151161e
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2016-06-18 21:34:41 +0200

    ioctl: use _IO* macros to generate ioctl numbers
    This provides a more convenient way to specify IOCTL numbers.
    
    from include/uapi/linux/btrfs.h:
    
        #define BTRFS_IOC_SPACE_INFO _IOWR(BTRFS_IOCTL_MAGIC, 20, \
            struct btrfs_ioctl_space_args)
    
    ...now translates to...
    
        IOC_SPACE_INFO = _IOWR(BTRFS_IOCTL_MAGIC, 20, ioctl_space_args)
    
    ...in python.
    
    TODO: x86 specific constants...

--- btrfs/ioctl.py
+++ btrfs/ioctl.py
@@ -26,15 +26,52 @@ import struct
 ULLONG_MAX = (1 << 64) - 1
 ULONG_MAX = (1 << 32) - 1
 
+BTRFS_IOCTL_MAGIC = 0x94
+
+_IOC_NRBITS = 8
+_IOC_TYPEBITS = 8
+_IOC_SIZEBITS = 14
+_IOC_DIRBITS = 2
+
+_IOC_NRSHIFT = 0
+_IOC_TYPESHIFT = _IOC_NRSHIFT + _IOC_NRBITS
+_IOC_SIZESHIFT = _IOC_TYPESHIFT + _IOC_TYPEBITS
+_IOC_DIRSHIFT = _IOC_SIZESHIFT + _IOC_SIZEBITS
+
+_IOC_NONE = 0
+_IOC_WRITE = 1
+_IOC_READ = 2
+
+
+def _IOC(_dir, _type, nr, size):
+    return (_dir << _IOC_DIRSHIFT) | (_type << _IOC_TYPESHIFT) | \
+        (nr << _IOC_NRSHIFT) | (size << _IOC_SIZESHIFT)
+
+
+def _IO(_type, nr):
+    return _IOC(_IOC_NONE, _type, nr, 0)
+
+
+def _IOR(_type, nr, _struct):
+    return _IOC(_IOC_READ, _type, nr, _struct.size)
+
+
+def _IOW(_type, nr, _struct):
+    return _IOC(_IOC_WRITE, _type, nr, _struct.size)
+
+
+def _IOWR(_type, nr, _struct):
+    return _IOC(_IOC_READ | _IOC_WRITE, _type, nr, _struct.size)
+
 
 def create_buf(size=4096):
     return array.array("B", itertools.repeat(0, size))
 
 
-IOC_SPACE_INFO = 0xc0109414
-
 ioctl_space_args = struct.Struct("=2Q")
 ioctl_space_info = struct.Struct("=3Q")
+
+IOC_SPACE_INFO = _IOWR(BTRFS_IOCTL_MAGIC, 20, ioctl_space_args)
 
 SpaceArgs = namedtuple('SpaceArgs', ['space_slots', 'total_spaces'])
 SpaceInfo = namedtuple('SpaceInfo', ['flags', 'total_bytes', 'used_bytes'])
@@ -56,10 +93,12 @@ def space_info(fd):
             for pos in xrange(ioctl_space_args.size, buf_size, ioctl_space_info.size)]
 
 
-IOC_TREE_SEARCH = 0xd0009411
-
 ioctl_search_key = struct.Struct("=Q6QLLL4x32x")
+ioctl_search_args = struct.Struct("{0}{1}x".format(
+    ioctl_search_key.format, 4096 - ioctl_search_key.size))
 ioctl_search_header = struct.Struct("=3Q2L")
+
+IOC_TREE_SEARCH = _IOWR(BTRFS_IOCTL_MAGIC, 17, ioctl_search_args)
 
 SearchHeader = namedtuple('SearchHeader', ['transid', 'objectid', 'offset', 'type', 'len'])
 
