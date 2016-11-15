d92d90bd8f5f33425d5c19122c14d0294dcd4efb
Hans van Kranenburg <hans.van.kranenburg@mendix.com>
2016-11-15 22:58:16 +0100

    Release v0.3

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open('README.pypi') as f:
 setup(
     name='btrfs',
     packages=['btrfs'],
-    version='0.2.1',
+    version='0.3',
     description='Python module to inspect btrfs filesystems.',
     long_description=long_description,
     author='Hans van Kranenburg',
     author_email='hans.van.kranenburg@mendix.com',
     url='https://github.com/knorrie/python-btrfs',
-    download_url='https://github.com/knorrie/python-btrfs/tarball/v0.2.1',
+    download_url='https://github.com/knorrie/python-btrfs/tarball/v0.3',
     keywords=['btrfs', 'filesystem'],
     classifiers=[
         'Development Status :: 3 - Alpha',
