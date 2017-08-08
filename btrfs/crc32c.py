72f28c4281d5fb4eb4cb74abe2be6871eb615c19
Nikolay Borisov <nborisov@suse.com>
2017-08-08 10:00:22 +0300

    crc32c: Improve crc32c implementation
    Add a new function crc32c_data which calculates the corect checksum of a
    bytes/bytesarray being passed to it. It uses the core crc32c implementation
    and implements the equivalent of btrfs_csum_final e.g. reversing the bits
    of the final checksum
    
    Also add an explicit truncation to 32bit value of the seed passed to crc32c
    function. This allows to ~1 and ~0 as seed values rather than manually calculating
    their 32bit equivalents.

--- btrfs/crc32c.py
+++ btrfs/crc32c.py
@@ -87,10 +87,13 @@ table = (
 def crc32c(crc, data):
     if not isinstance(data, (bytes, bytearray)):
         data = bytes(data, 'utf-8')
+    crc = crc & 0xffffffff
     for char in data:
         crc = table[(crc ^ char) & 0xff] ^ (crc >> 8)
     return crc & 0xffffffff
 
+def crc32c_data(data):
+    return 0xffffffff ^ crc32c(~0, data)
 
 def name_hash(name):
     return crc32c(4294967294, name)  # (u32)~1
