1c23894514d4edbe18d1d28bc5325f79aab3676c
Hans van Kranenburg <hans@knorrie.org>
2019-07-28 01:17:15 +0200

    examples/show_subvolumes: show subvolume paths
    Change the example to use the newly added RootRef to make the output
    list all locations of subvolumes. This is very similar to the output of
    doing btrfs sub list -a.
    
    -# ./show_subvolumes.py .
    ID 259 parent 5 path /rootfs
    ID 1052 parent 259 path /rootfs/var/lib/machines
    ID 260 parent 5 path /home
    ID 2454 parent 260 path /home/knorrie/tmp
    ID 852 parent 5 path /zooi
    ID 5167 parent 5 path /.snapshot/rootfs.20190624T222913+0200
    ID 5168 parent 5 path /.snapshot/home.20190624T222913+0200
    [...]

--- examples/show_subvolumes.py
+++ examples/show_subvolumes.py
@@ -7,6 +7,23 @@ if len(sys.argv) < 2:
     print("Usage: {} <mountpoint>".format(sys.argv[0]))
     sys.exit(1)
 
+
+def subvolumes_inside(fs, parent_tree):
+    min_key = btrfs.ctree.Key(parent_tree, btrfs.ctree.ROOT_REF_KEY, 0)
+    max_key = btrfs.ctree.Key(parent_tree, btrfs.ctree.ROOT_REF_KEY + 1, 0) - 1
+    for header, data in btrfs.ioctl.search_v2(fs.fd, 1, min_key, max_key):
+        ref = btrfs.ctree.RootRef(header, data)
+        path = (btrfs.ioctl.ino_lookup(fs.fd, ref.parent_tree, ref.dirid).name_bytes +
+                ref.name).decode()
+        yield ref.tree, path
+
+
+def print_subvolumes_inside(fs, parent_tree, parent_path):
+    for tree, path in subvolumes_inside(fs, parent_tree):
+        sub_path = "{}/{}".format(parent_path, path)
+        print("ID {} parent {} path {}".format(tree, parent_tree, sub_path))
+        print_subvolumes_inside(fs, tree, sub_path)
+
+
 with btrfs.FileSystem(sys.argv[1]) as fs:
-    for subvol in fs.subvolumes():
-        print(subvol)
+    print_subvolumes_inside(fs, 5, '')
