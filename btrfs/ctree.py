knorrie/python-btrfs

commit 2916bbf54d32a984fe1df30fe3bc52f770fa9518

Search ioctl, search key, devices, chunks and stripes
A filesystem has devices and chunks, they're listed in the chunk tree.
In order to be able to list them, the search ioctl needs to be used.

This commit adds the search ioctl operation and handling of the search
key. The FileSystem object can now be queried for devices and chunks
with their stripes.

Searching for a block group in the extent tree can be done by using the
vaddr field from a chunk.

examples/show_usage.py lists all the physical locations of chunks for a
filesystem. By looking up the block group that the chunk belongs to, we
can show what percentage of a chunk is actually in use by data. Works
for single and RAID1 now, probably not for more fancy raid types yet.

btrfs/ctree.py
@@ -16,9 +16,22 @@
 # Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 # Boston, MA 021110-1307, USA.
 
+from btrfs.ioctl import ULLONG_MAX
 import btrfs.ioctl
+import copy
 import os
+import struct
+import uuid
 
+
+DEV_ITEMS_OBJECTID = 1
+EXTENT_TREE_OBJECTID = 2
+CHUNK_TREE_OBJECTID = 3
+FIRST_CHUNK_TREE_OBJECTID = 256
+
+BLOCK_GROUP_ITEM_KEY = 192
+DEV_ITEM_KEY = 216
+CHUNK_ITEM_KEY = 228
 
 BLOCK_GROUP_DATA = 1 << 0
 BLOCK_GROUP_SYSTEM = 1 << 1
@@ -47,6 +60,91 @@ BLOCK_GROUP_PROFILE_MASK = (
 SPACE_INFO_GLOBAL_RSV = 1 << 49
 
 
+class Key(object):
+    def __init__(self, objectid, _type, offset):
+        self._objectid = objectid
+        self._type = _type
+        self._offset = offset
+        self._pack()
+
+    @property
+    def objectid(self):
+        return self._objectid
+
+    @objectid.setter
+    def objectid(self, _objectid):
+        self._objectid = _objectid
+        self._pack()
+
+    @property
+    def type(self):
+        return self._type
+
+    @type.setter
+    def type(self, _type):
+        self._type = _type
+        self._pack()
+
+    @property
+    def offset(self):
+        return self._offset
+
+    @offset.setter
+    def offset(self, _offset):
+        self._offset = _offset
+        self._pack()
+
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, _key):
+        self._key = _key
+        self._unpack()
+
+    def _pack(self):
+        self._key = (self.objectid << 72) + (self._type << 64) + self.offset
+
+    def _unpack(self):
+        self._objectid = self._key >> 72
+        self._type = (self._key & ((1 << 72) - 1)) >> 64
+        self._offset = (self._key & ((1 << 64) - 1))
+
+    def __lt__(self, other):
+        if isinstance(other, Key):
+            return self._key < other._key
+        return self._key < other
+
+    def __le__(self, other):
+        if isinstance(other, Key):
+            return self._key <= other._key
+        return self._key <= other
+
+    def __eq__(self, other):
+        if isinstance(other, Key):
+            return self._key == other._key
+        return self._key == other
+
+    def __ge__(self, other):
+        if isinstance(other, Key):
+            return self._key >= other._key
+        return self._key >= other
+
+    def __gt__(self, other):
+        if isinstance(other, Key):
+            return self._key > other._key
+        return self._key > other
+
+    def __str__(self):
+        return "({0} {1} {2})".format(self._objectid, self._type, self._offset)
+
+    def __add__(self, amount):
+        new_key = copy.copy(self)
+        new_key.key += amount
+        return new_key
+
+
 class FileSystem(object):
     def __init__(self, path):
         self.path = path
@@ -54,3 +152,69 @@ class FileSystem(object):
 
     def space_info(self):
         return btrfs.ioctl.space_info(self.fd)
+
+    def devices(self):
+        tree = CHUNK_TREE_OBJECTID
+        min_key = Key(DEV_ITEMS_OBJECTID, DEV_ITEM_KEY, 0)
+        max_key = Key(DEV_ITEMS_OBJECTID, DEV_ITEM_KEY, ULLONG_MAX)
+        for header, data in btrfs.ioctl.search(self.fd, tree, min_key, max_key):
+            yield Device(header, data)
+
+    def chunks(self):
+        tree = CHUNK_TREE_OBJECTID
+        min_key = Key(FIRST_CHUNK_TREE_OBJECTID, CHUNK_ITEM_KEY, 0)
+        max_key = Key(FIRST_CHUNK_TREE_OBJECTID, CHUNK_ITEM_KEY, ULLONG_MAX)
+        for header, data in btrfs.ioctl.search(self.fd, tree, min_key, max_key):
+            yield Chunk(header, data)
+
+    def block_group(self, vaddr):
+        tree = EXTENT_TREE_OBJECTID
+        min_key = Key(vaddr, BLOCK_GROUP_ITEM_KEY, 0)
+        max_key = Key(vaddr, BLOCK_GROUP_ITEM_KEY, ULLONG_MAX)
+        block_groups = [BlockGroup(header, data)
+                        for header, data in
+                        btrfs.ioctl.search(self.fd, tree, min_key, max_key, nr_items=1)]
+        return block_groups[0]
+
+
+class Device(object):
+    dev_item = struct.Struct("<3Q3L3QL2B16s16s")
+
+    def __init__(self, header, data):
+        self.devid, self.total_bytes, self.bytes_used, self.io_align, self.io_width, \
+            self.sector_size, self.type, self.generation, self.start_offset, self.dev_group, \
+            self.seek_speed, self.bandwidth, uuid_bytes, fsid_bytes = \
+            Device.dev_item.unpack_from(data, 0)
+        self.uuid = uuid.UUID(bytes=uuid_bytes)
+        self.fsid = uuid.UUID(bytes=fsid_bytes)
+
+
+class Chunk(object):
+    chunk = struct.Struct("<4Q3L2H")
+
+    def __init__(self, header, data):
+        self.vaddr = header.offset
+        self.length, self.owner, self.stripe_len, self.type, self.io_align, \
+            self.io_width, self.sector_size, self.num_stripes, self.sub_stripes = \
+            Chunk.chunk.unpack_from(data, 0)
+        pos = Chunk.chunk.size
+        self.stripes = [Stripe(data, stripe_pos)
+                        for stripe_pos in xrange(pos,
+                                                 pos + Stripe.stripe.size * self.num_stripes,
+                                                 Stripe.stripe.size)]
+
+
+class Stripe(object):
+    stripe = struct.Struct("<2Q16s")
+
+    def __init__(self, data, pos=0):
+        self.devid, self.offset, uuid_bytes = Stripe.stripe.unpack_from(data, pos)
+        self.uuid = uuid.UUID(bytes=uuid_bytes)
+
+
+class BlockGroup(object):
+    block_group_item = struct.Struct("<3Q")
+
+    def __init__(self, header, data):
+        self.used, self.chunk_objectid, self.flags = \
+            BlockGroup.block_group_item.unpack_from(data, 0)

btrfs/ioctl.py
@@ -18,9 +18,13 @@
 
 from collections import namedtuple
 import array
+import btrfs.ctree
 import fcntl
 import itertools
 import struct
+
+ULLONG_MAX = (1 << 64) - 1
+ULONG_MAX = (1 << 32) - 1
 
 
 def create_buf(size=4096):
@@ -50,3 +54,39 @@ def space_info(fd):
     fcntl.ioctl(fd, IOC_SPACE_INFO, buf)
     return [SpaceInfo(*ioctl_space_info.unpack_from(buf, pos))
             for pos in xrange(ioctl_space_args.size, buf_size, ioctl_space_info.size)]
+
+
+IOC_TREE_SEARCH = 0xd0009411
+
+ioctl_search_key = struct.Struct("=Q6QLLL4x32x")
+ioctl_search_header = struct.Struct("=3Q2L")
+
+SearchHeader = namedtuple('SearchHeader', ['transid', 'objectid', 'offset', 'type', 'len'])
+
+
+def search(fd, tree, min_key, max_key=None,
+           transid_min=0, transid_max=ULLONG_MAX,
+           nr_items=ULONG_MAX):
+    if max_key is None:
+        max_key = btrfs.ctree.Key(ULLONG_MAX, 255, ULLONG_MAX)
+    result_nr_items = -1
+    buf = create_buf(4096)
+    while min_key <= max_key and result_nr_items != 0:
+        ioctl_search_key.pack_into(buf, 0, tree,
+                                   min_key.objectid, max_key.objectid,
+                                   min_key.offset, max_key.offset,
+                                   transid_min, transid_max,
+                                   min_key.type, max_key.type,
+                                   nr_items)
+        fcntl.ioctl(fd, IOC_TREE_SEARCH, buf)
+        result_nr_items = ioctl_search_key.unpack_from(buf, 0)[9]
+        if result_nr_items > 0:
+            pos = ioctl_search_key.size
+            for i in xrange(result_nr_items):
+                header = SearchHeader(*ioctl_search_header.unpack_from(buf, pos))
+                pos += ioctl_search_header.size
+                data = buf[pos:pos+header.len]
+                yield((header, data))
+                pos += header.len
+            min_key = btrfs.ctree.Key(header.objectid, header.type, header.offset)
+            min_key += 1

examples/show_usage.py
@@ -1,0 +1,21 @@
+#!/usr/bin/python
+
+from __future__ import print_function
+import btrfs
+import sys
+
+fs = btrfs.FileSystem(sys.argv[1])
+for device in fs.devices():
+    print("dev item devid {0} total bytes {1} bytes used {2}".format(
+        device.devid, device.total_bytes, device.bytes_used))
+
+for chunk in fs.chunks():
+    block_group = fs.block_group(chunk.vaddr)
+    used = block_group.used
+    used_pct = (used * 100) / chunk.length
+    for i in xrange(len(chunk.stripes)):
+        stripe = chunk.stripes[i]
+        print("chunk vaddr {0} type {1} stripe {2} devid {3} offset {4} length {5} "
+              "used {6} used_pct {7}".format(
+                  chunk.vaddr, chunk.type, i, stripe.devid, stripe.offset,
+                  chunk.length, used, used_pct))

commit de8fb97a3242b335a7ab05f5cc04c4907e953c75

ctree: fix key value wrap behaviour
We can do fun stuff with keys, like this:

    >>> k = btrfs.ctree.Key(0, 0, 0) - 1
    >>> k
    Key(18446744073709551615, 255, 18446744073709551615)

However... what's the actual 136 bit key value in this case?

    >>> k.key
    -1

That's not good, because -1 is not a valid unsigned 136 bit number at
all. That should be:

    >> btrfs.ctree.Key(-1, -1, -1).key
    87112285931760246646623899502532662132735

So, when using  btrfs.ctree.Key(0, 0, 0) - 1 as max key value for a
search, only the values from the first search ioctl call result buffer
would be returned, and then the search would stop.

Same bug is present for wrapping the value the other way round:

    >>> l = btrfs.ctree.Key(18446744073709551615, 255, 18446744073709551615) + 1
    >>> l
    Key(0, 0, 0)
    >>> l.key
    87112285931760246646623899502532662132736

So, we have to manually reduce the value to 136 bits again, since python
has no idea about different sized number storage.

Fixes: 2916bbf54d ("Search ioctl, search key, devices, chunks and stripes")

btrfs/ctree.py
@@ -438,6 +438,9 @@ class ItemNotFoundError(IndexError):
     pass
 
 
+KEY_MAX = (1 << 136) - 1
+
+
 class Key(object):
     """Btrfs metadata trees have a key space of 136-bit numbers.
 
@@ -536,7 +539,7 @@ class Key(object):
 
     @key.setter
     def key(self, _key):
-        self._key = _key
+        self._key = _key & KEY_MAX
         self._unpack()
 
     def _pack(self):